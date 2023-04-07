511e33bf877eca53db1cb2777d6590d1313248cc
jeth318 <jesper.thornberg@me.com>
2023-04-07 21:40:57 +0200

    WIP

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,11 +6,11 @@ export const Header = () => {
 
   return (
     <div className="navbar bg-primary text-primary-content">
-      <div className="test-3xl flex-1 pl-5 font-bold">Beach bookings</div>
+      <div className="flex-1 pl-1 text-2xl font-bold">Beach bookings</div>
       <div className="btn-group btn">
         {sessionData?.user.id && (
-          <button className="btn-add btn-success btn-sm btn font-bold text-white">
-            <Link href="/booking">Add</Link>
+          <button className="btn-add btn-success btn-sm btn font-bold">
+            <Link href="/booking">New booking</Link>
           </button>
         )}
         <button
