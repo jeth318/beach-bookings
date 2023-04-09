3425cb768d6c44a317d9a1c65fcb4de0662cb7d2
jeth318 <jesper.thornberg@me.com>
2023-04-09 13:47:52 +0200

    Add users table to gui

--- src/components/PlayersTable.tsx
+++ src/components/PlayersTable.tsx
@@ -1,15 +1,76 @@
-import { type User } from "@prisma/client";
-import { useSession } from "next-auth/react";
+import { type Booking, type User } from "@prisma/client";
 import Image from "next/image";
+import { useState } from "react";
+import { BeatLoader } from "react-spinners";
+import { api } from "~/utils/api";
 
 type Props = {
-  players: User[];
+  booking: Booking;
 };
 
-export const PlayersTable = ({ players }: Props) => {
+export const PlayersTable = ({ booking }: Props) => {
+  const { refetch: refetchBookings } = api.booking.getAll.useQuery();
+  const updateBooking = api.booking.update.useMutation();
+  const {
+    data: users,
+    isFetching: isFetchingUsers,
+    isRefetching: isRefetchingUsers,
+  } = api.user.getAll.useQuery();
+  const [playerToRemove, setPlayerToRemove] = useState<string | undefined>();
+  console.log("isfetching", isFetchingUsers);
+
+  const playersInBooking =
+    users?.filter((user) => booking.players.includes(user.id)) || [];
+
+  const removePlayer = (playerId: string) => {
+    console.log({ playerId });
+
+    updateBooking.mutate({
+      ...booking,
+      players: booking.players.filter((id) => id !== playerId),
+    });
+    setPlayerToRemove(undefined);
+    setTimeout(() => {
+      void refetchBookings();
+    }, 1000);
+  };
   return (
-    <div className="">
-      <table className="table-compact table w-full">
+    <div>
+      <input
+        type="checkbox"
+        id="action-modal-player-remove"
+        className="modal-toggle"
+      />
+      <div className="modal modal-bottom sm:modal-middle">
+        <div className="modal-box">
+          <h3 className="text-lg font-bold">Confirm kick 🦵👋</h3>
+          <p className="py-4">
+            If you remove this player from this booking, he or she will have to
+            re-join them selfes.
+          </p>
+          <div>
+            <div className="btn-group">
+              <label
+                htmlFor="action-modal-player-remove"
+                className="btn text-white"
+              >
+                Cancel
+              </label>
+              <label
+                htmlFor="action-modal-player-remove"
+                className="btn-error btn text-white"
+                onClick={() => {
+                  !!playerToRemove && removePlayer(playerToRemove);
+                }}
+              >
+                Remove player
+              </label>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <table className="table-compact collapse table w-full overflow-x-auto">
         {/* head */}
         <thead>
           <tr>
@@ -18,33 +79,49 @@ export const PlayersTable = ({ players }: Props) => {
           </tr>
         </thead>
         <tbody>
-          {players.map((player) => {
-            return (
-              <tr key={player.id}>
-                <td>
-                  <div className="flex items-center space-x-3">
-                    <div className="avatar">
-                      <div className="mask mask-squircle h-12 w-12">
-                        <Image
-                          height={100}
-                          width={100}
-                          alt="user-icon-default"
-                          src={player.image || "/user-default.png"}
-                        />
+          {!playersInBooking || isFetchingUsers ? (
+            <tr>
+              <td colSpan={2}>
+                <div className="flex justify-center">
+                  <BeatLoader size={20} color="#36d7b7" />
+                </div>
+              </td>
+            </tr>
+          ) : (
+            playersInBooking?.map((player) => {
+              return (
+                <tr key={player.id}>
+                  <td>
+                    <div className="flex items-center space-x-3">
+                      <div className="avatar">
+                        <div className="mask mask-squircle h-12 w-12">
+                          <Image
+                            height={100}
+                            width={100}
+                            alt="user-icon-default"
+                            src={player.image || "/user-default.png"}
+                          />
+                        </div>
+                      </div>
+                      <div>
+                        <div className="font-bold">{player.name}</div>
+                        <div className="text-sm opacity-50">{player.email}</div>
                       </div>
                     </div>
-                    <div>
-                      <div className="font-bold">{player.name}</div>
-                      <div className="text-sm opacity-50">{player.email}</div>
-                    </div>
-                  </div>
-                </td>
-                <td>
-                  <button className="btn-outline btn-sm btn">Remove</button>
-                </td>
-              </tr>
-            );
-          })}
+                  </td>
+                  <td style={{ textAlign: "center" }}>
+                    <label
+                      onClick={() => setPlayerToRemove(player.id)}
+                      className="btn-outline btn-sm btn"
+                      htmlFor="action-modal-player-remove"
+                    >
+                      Kick 👋
+                    </label>
+                  </td>
+                </tr>
+              );
+            })
+          )}
         </tbody>
         {/* foot */}
         <tfoot>
--- src/pages/booking.tsx
+++ src/pages/booking.tsx
@@ -12,8 +12,9 @@ import Link from "next/link";
 import { PlayersTable } from "~/components/PlayersTable";
 
 const Booking: NextPage = () => {
-  const { data: sessionData } = useSession();
-  const { data: users } = api.user.getAll.useQuery();
+  const { data: sessionData, status: sessionStatus } = useSession();
+  const { data: users, isInitialLoading: isInitialLoadingUsers } =
+    api.user.getAll.useQuery();
   const router = useRouter();
   const [bookingToEdit, setBookingToEdit] = useState<Booking>();
   const [court, setCourt] = useState<number | null>();
@@ -28,8 +29,11 @@ const Booking: NextPage = () => {
     setTime(undefined);
   };
 
-  const { data: bookings, refetch: refetchBookings } =
-    api.booking.getAll.useQuery();
+  const {
+    data: bookings,
+    refetch: refetchBookings,
+    isInitialLoading: isInitialLoadingBookings,
+  } = api.booking.getAll.useQuery();
   const createBooking = api.booking.create.useMutation({});
   const updateBooking = api.booking.update.useMutation({});
 
@@ -58,6 +62,19 @@ const Booking: NextPage = () => {
       resetBooking();
     }
   }, [bookingToEdit, bookings, router.query.booking]);
+
+  const isInitialLoading =
+    isInitialLoadingBookings ||
+    isInitialLoadingUsers ||
+    sessionStatus === "loading";
+
+  console.log(sessionStatus);
+
+  const defaultBooking = {
+    players: [sessionData?.user.id],
+    court: null,
+    userId: sessionData?.user.id,
+  } as Booking;
 
   const addBooking = () => {
     if (!validBooking) {
@@ -106,116 +123,116 @@ const Booking: NextPage = () => {
       </Head>
       <Header />
       <main className="min-w-sm pd-3 flex min-w-fit flex-col bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        {sessionData?.user.id ? (
-          <div className="container p-4">
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text text-white">When</span>
-              </label>
-              <label className="input-group">
-                <span>Date</span>
-                <input
-                  onChange={({ target }) => {
-                    console.log("target.value", target.value);
-                    setDate(target.value);
-                  }}
-                  defaultValue={date}
-                  type="date"
-                  className="input-bordered input"
-                />
-                <input
-                  defaultValue={time}
-                  onChange={({ target }) => {
-                    console.log("TIME", target.value);
-
-                    setTime(target.value);
-                  }}
-                  type="time"
-                  className="input-bordered input"
-                />
-              </label>
-
-              <br />
-              <label className="label">
-                <span className="label-text text-white">Duration</span>
-              </label>
-              <div className="btn-group">
-                <button
-                  onClick={(e) => {
-                    const target = e.target as HTMLTextAreaElement;
-
-                    setDuration(parseInt(target.value));
-                  }}
-                  className={`btn ${duration === 60 ? "btn-active" : ""}`}
-                  value={60}
-                >
-                  60 min
-                </button>
-                <button
-                  onClick={(e) => {
-                    const target = e.target as HTMLTextAreaElement;
-                    setDuration(parseInt(target.value));
-                  }}
-                  className={`btn ${duration === 90 ? "btn-active" : ""}`}
-                  value={90}
-                >
-                  90 min
-                </button>
-              </div>
-              <br />
-              <label className="label">
-                <span className="label-text text-white">Where</span>
-              </label>
-              <label className="input-group">
-                <span>Court</span>
-                <select
-                  className="select-bordered select"
-                  onChange={(val) => {
-                    setCourt(parseInt(val.target.value));
-                  }}
-                  value={court || "Pick court"}
-                >
-                  <option disabled>Pick court</option>
-                  <option>1</option>
-                  <option>2</option>
-                  <option>3</option>
-                  <option>4</option>
-                  <option>5</option>
-                  <option>6</option>
-                  <option>7</option>
-                  <option>8</option>
-                  <option>9</option>
-                  <option>11</option>
-                  <option>12</option>
-                </select>
-              </label>
-              <br />
-              <label className="label">
-                <span className="label-text text-white">Players</span>
-              </label>
-              {!!bookingToEdit && (
-                <PlayersTable players={getPlayers(bookingToEdit)} />
-              )}
-              <div className="btn-group btn-group-vertical pt-5">
-                <Link className="btn-warning btn" href="/">
-                  Back
-                </Link>
-                <button
-                  onClick={addBooking}
-                  className={`${
-                    validBooking ? "btn-success" : "btn-disabled"
-                  } btn text-white`}
-                >
-                  Confirm
-                </button>
-              </div>
-            </div>
-          </div>
-        ) : (
-          <div className="flex flex-col items-center justify-center p-3">
+        {!isInitialLoading && sessionStatus === "unauthenticated" ? (
+          <div className="flex h-screen flex-col items-center justify-center p-3">
             <h2 className="text-center text-2xl text-white">
               If you want to add or edit bookings, you have to be logged in.
             </h2>
+          </div>
+        ) : (
+          <div className="container p-4">
+            {sessionData?.user.id && (
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text text-white">When</span>
+                </label>
+                <label className="input-group">
+                  <span>Date</span>
+                  <input
+                    onChange={({ target }) => {
+                      console.log("target.value", target.value);
+                      setDate(target.value);
+                    }}
+                    defaultValue={date}
+                    type="date"
+                    className="input-bordered input"
+                  />
+                  <input
+                    defaultValue={time}
+                    onChange={({ target }) => {
+                      console.log("TIME", target.value);
+
+                      setTime(target.value);
+                    }}
+                    type="time"
+                    className="input-bordered input"
+                  />
+                </label>
+
+                <br />
+                <label className="label">
+                  <span className="label-text text-white">Duration</span>
+                </label>
+                <div className="btn-group">
+                  <button
+                    onClick={(e) => {
+                      const target = e.target as HTMLTextAreaElement;
+
+                      setDuration(parseInt(target.value));
+                    }}
+                    className={`btn ${duration === 60 ? "btn-active" : ""}`}
+                    value={60}
+                  >
+                    60 min
+                  </button>
+                  <button
+                    onClick={(e) => {
+                      const target = e.target as HTMLTextAreaElement;
+                      setDuration(parseInt(target.value));
+                    }}
+                    className={`btn ${duration === 90 ? "btn-active" : ""}`}
+                    value={90}
+                  >
+                    90 min
+                  </button>
+                </div>
+                <br />
+                <label className="label">
+                  <span className="label-text text-white">Where</span>
+                </label>
+                <label className="input-group">
+                  <span>Court</span>
+                  <select
+                    className="select-bordered select"
+                    onChange={(val) => {
+                      setCourt(parseInt(val.target.value));
+                    }}
+                    value={court || "Pick court"}
+                  >
+                    <option disabled>Pick court</option>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                    <option>6</option>
+                    <option>7</option>
+                    <option>8</option>
+                    <option>9</option>
+                    <option>11</option>
+                    <option>12</option>
+                  </select>
+                </label>
+                <br />
+                <label className="label">
+                  <span className="label-text text-white">Players</span>
+                </label>
+                <PlayersTable booking={bookingToEdit || defaultBooking} />
+                <div className="btn-group btn-group-vertical pt-5">
+                  <Link className="btn-warning btn" href="/">
+                    Back
+                  </Link>
+                  <button
+                    onClick={addBooking}
+                    className={`${
+                      validBooking ? "btn-success" : "btn-disabled"
+                    } btn text-white`}
+                  >
+                    Confirm
+                  </button>
+                </div>
+              </div>
+            )}
           </div>
         )}
       </main>
