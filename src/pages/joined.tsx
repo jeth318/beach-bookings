21c6a8988cd9fe01f7191dae71c0f19afc8b9041
jeth318 <jesper.thornberg@me.com>
2023-04-11 10:41:37 +0200

    Remove h-screen from main

--- src/pages/joined.tsx
+++ src/pages/joined.tsx
@@ -30,7 +30,7 @@ const Joined: NextPage = () => {
           </div>
         </div>
       ) : (
-        <main className="h-screen bg-slate-200 dark:bg-black">
+        <main className="bg-slate-200 dark:bg-black">
           <div className="min-w-sm flex min-w-fit flex-col bg-gradient-to-b from-[#2e026d] to-[#15162c]">
             <Bookings joinedOnly />
           </div>
