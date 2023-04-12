c75a8af4fe9eda00c3eaff0054baefb90ec3ea46
jeth318 <jesper.thornberg@me.com>
2023-04-12 23:42:40 +0200

    Speed up

---
+++ src/components/PageLoader.tsx
@@ -0,0 +1,14 @@
+import { BeatLoader } from "react-spinners";
+
+type Props = { isMainPage: boolean; bgColor: string; mainBgColor: string };
+export const PageLoader = ({ isMainPage, bgColor, mainBgColor }: Props) => {
+  return (
+    <div
+      className={`${
+        isMainPage ? "bg-min-height-no-submenu" : "bg-min-height"
+      } flex items-start justify-center ${isMainPage ? mainBgColor : bgColor}`}
+    >
+      <BeatLoader className="pt-32" color="#36d7b7" size={25} />
+    </div>
+  );
+};
