5e07dabce8f38b70844fc288520adf02063a1843
jeth318 <jesper.thornberg@me.com>
2023-04-17 16:45:25 +0200

    Add more courts

--- src/pages/booking.tsx
+++ src/pages/booking.tsx
@@ -290,6 +290,21 @@ const Booking = () => {
                     <option>10</option>
                     <option>11</option>
                     <option>12</option>
+                    <option>13</option>
+                    <option>14</option>
+                    <option>15</option>
+                    <option>16</option>
+                    <option>17</option>
+                    <option>18</option>
+                    <option>19</option>
+                    <option>20</option>
+                    <option>21</option>
+                    <option>22</option>
+                    <option>23</option>
+                    <option>24</option>
+                    <option>25</option>
+                    <option>26</option>
+                    <option>27</option>
                   </select>
                 </label>
                 <label className="label">
