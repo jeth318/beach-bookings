43fc5540941b1dce1a1af8d75be205ef40d2ff32
jeth318 <jesper.thornberg@me.com>
2023-04-21 15:09:46 +0200

    Revert to old svg for burger menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -59,11 +59,20 @@ export const Header = () => {
                 tabIndex={0}
                 className="btn-ghost btn-circle btn"
               >
-                <CustomIcon
-                  height={25}
-                  width={25}
-                  path={`/svg/burger-menu.svg`}
-                />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h7"
+                  />
+                </svg>
               </label>
               <ul
                 tabIndex={0}
