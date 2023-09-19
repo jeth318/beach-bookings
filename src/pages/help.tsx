b212a65e2001afd2c154dd5a3025c3915513bc7f
jeth318 <jesper.thornberg@me.com>
2023-09-19 13:32:18 +0200

    Improve how to

--- src/pages/help.tsx
+++ src/pages/help.tsx
@@ -18,7 +18,7 @@ const Help = () => {
       <div
         style={{
           padding: 10,
-          paddingTop: 30,
+          paddingTop: 70,
           maxWidth: 600,
         }}
       >
@@ -31,38 +31,29 @@ const Help = () => {
         </h1>
         <br />
         <p className="text-lg font-bold text-gray-500 dark:text-gray-400 lg:text-xl">
-          🌴 Add a new booking
+          🌴 Publish a booking
         </p>
         <p className="pl-1 pl-2 pl-4 pl-6 text-lg font-normal text-gray-500 dark:text-gray-400 lg:text-xl">
-          1️⃣ Start by visiting the{" "}
+          1️⃣ Visit the{" "}
           <a className="link" href="https://gbc.goactivebooking.com/">
             official GBC website
           </a>{" "}
-          and proceed with your booking as you normally would. It is essential
-          to receive the confirmation email for your booking from GBC before
-          proceeding to the next step. Nobody wants the disappointment of
-          arriving at the facility only to find out there is no reservation on
-          record!
+          , place a regular booking and wait for their confirmation email.
         </p>
         <br />
         <p className="pl-1 pl-2 pl-4 pl-6 text-lg font-normal text-gray-500 dark:text-gray-400 lg:text-xl">
-          2️⃣ Now, go back and log in to Beach Bookings and navigate to the top
-          left menu where you will find the Add-option.
+          2️⃣ Here in Beach Bookings, navigate to the top left menu where you
+          will find the Add-option. Tap it.
         </p>
         <br />
         <p className="pl-1 pl-2 pl-4 pl-6 text-lg font-normal text-gray-500 dark:text-gray-400 lg:text-xl">
-          3️⃣ Select Add and then proceed to input all the necessary details from
-          your GBC booking. This includes information like the date, time,
-          facility (only GBC Kviberg available at the moment), court, your group
-          (which is currently one big group), and the number of players needed
-          or allowed (usually defaults to 4).
+          3️⃣ Fill in the details from your GBC booking along with the amount of
+          players required/allowed (defaults to 4).
         </p>
         <br />
         <p className="pl-1 pl-2 pl-4 pl-6 text-lg font-normal text-gray-500 dark:text-gray-400 lg:text-xl">
-          4️⃣ When all required information is entered, the Save-button will turn
-          green. Click save and your now booking will be added and everyone will
-          get notified via email that you just added an opportunity to play.
-          Enjoy your game on the beach! 🏖️🏐
+          4️⃣ Click the green Save-button. All players will instantly receive an
+          email with info about the new booking.
         </p>
         <br />
         <hr />
@@ -71,8 +62,9 @@ const Help = () => {
           🌴 Join booking
         </p>
         <p className="pl-1 pl-2 pl-4 pl-6 text-lg font-normal text-gray-500 dark:text-gray-400 lg:text-xl">
-          When logged in, simply tap Join in in the booking and confirm. When
-          joining, an email will be sent to the other players in the booking.
+          Log in and tap the green Join-button (if visible). After confirmation,
+          an email will be sent to the other players in the booking letting them
+          know about the new recruit.
         </p>
         <br />
         <hr />
@@ -81,10 +73,32 @@ const Help = () => {
           🌴 Leave booking
         </p>
         <p className="pl-1 pl-2 pl-4 pl-6 text-lg font-normal text-gray-500 dark:text-gray-400 lg:text-xl">
-          When logged in, go to Joined in the menu and tap and confirm on the
-          orange leave-button. Make sure to be logged in, otherwise the actions
-          such as leave wont be visible. When leaving, an email will be sent to
-          the other players in the booking.
+          Log in and tap the orange Leave-button (if visible). After
+          confirmation, an email will be sent to the other players in the
+          booking letting them know you left.
+        </p>
+        <br />
+        <hr />
+        <br />
+        <p className="text-lg font-bold text-gray-500 dark:text-gray-400 lg:text-xl">
+          🌴 Change/Edit booking
+        </p>
+        <p className="pl-1 pl-2 pl-4 pl-6 text-lg font-normal text-gray-500 dark:text-gray-400 lg:text-xl">
+          Log in and tap the top menu icon, then tap Booked-option (visible only
+          for bookings added by you). After confirmation, an email will be sent
+          to the other players in the booking with information of the update.
+        </p>
+        <br />
+        <hr />
+        <br />
+        <p className="text-lg font-bold text-gray-500 dark:text-gray-400 lg:text-xl">
+          🌴 Remove booking
+        </p>
+        <p className="pl-1 pl-2 pl-4 pl-6 text-lg font-normal text-gray-500 dark:text-gray-400 lg:text-xl">
+          Log in and tap the top menu icon, then tap Booked-option (visible only
+          for bookings added by you). Selet Delete-option After confirmation, an
+          email will be sent to the other players in the booking with
+          information of the update. Important!
         </p>
         <br />
         <hr />
@@ -97,8 +111,7 @@ const Help = () => {
           app...
         </p>
         <br />
-
-        <p className="text-lg font-normal italic text-gray-500 dark:text-gray-400 lg:text-xl">
+        <p className="p-2 text-lg font-normal italic text-gray-500 dark:text-gray-400 lg:text-xl">
           - Get ready to have a blast with Beach Bookings, the ultimate web app
           for player squad gathering. Say goodbye to boring Google Sheets and
           say hello to an interactive experience that keeps you in the loop.
