62f8e0892acd1fe728ddab243a2df85dec7ef610
jeth318 <jesper.thornberg@me.com>
2023-11-13 22:21:01 +0100

    Readd prev stylin

--- src/components/Bookings.tsx
+++ src/components/Bookings.tsx
@@ -350,28 +350,24 @@ export const Bookings = ({ bookings }: Props) => {
                         {booking.players.length === 4 && !historyOnly && " ✅"}
                       </h2>
                       <div className="flex flex-col pb-1 font-medium">
+                        <div className="flex flex-row self-start pb-2">
+                          <CustomIcon path="/svg/duration.svg" width={20} />
+                          <div className="pl-1 text-lg">
+                            {parseTime(booking)}
+                          </div>
+                        </div>
+
                         <div
-                          style={{ maxWidth: "200px" }}
+                          style={{ maxWidth: "150px" }}
                           className="transparent-background-grey self-start rounded-lg border border-slate-600 p-1"
                         >
-                          <div className="flex flex-row items-center self-start pb-1 ">
-                            <span className="pr-1">
-                              <CustomIcon path="/svg/duration.svg" width={20} />
-                            </span>
-                            <div
-                              style={{ maxWidth: 180 }}
-                              className="overflow-dots"
-                            >
-                              {parseTime(booking)}
-                            </div>
-                          </div>
                           {booking.associationId && (
                             <div className="flex flex-row items-center self-start pb-1 ">
                               <span className="pr-1">
                                 <CustomIcon path="/svg/people.svg" width={18} />
                               </span>
                               <div
-                                style={{ maxWidth: 180 }}
+                                style={{ maxWidth: 100 }}
                                 className="overflow-dots"
                               >
                                 {getAssociation(booking?.associationId)?.name}
