54bb6dac5596fd04522968c8387a9316483657fe
jeth318 <jesper.thornberg@me.com>
2024-03-28 07:36:31 +0100

    Add additional db connection string

--- src/env.mjs
+++ src/env.mjs
@@ -6,6 +6,7 @@ import { z } from "zod";
  */
 const server = z.object({
   DATABASE_URL: z.string().url(),
+  DATABASE_URL_SESSION: z.string().url(),
   NODE_ENV: z.enum(["development", "test", "production"]),
   NEXTAUTH_SECRET:
     process.env.NODE_ENV === "production"
@@ -51,6 +52,7 @@ const client = z.object({
  */
 const processEnv = {
   DATABASE_URL: process.env.DATABASE_URL,
+  DATABASE_URL_SESSION: process.env.DATABASE_URL_SESSION,
   NODE_ENV: process.env.NODE_ENV,
   NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
   NEXTAUTH_URL: process.env.NEXTAUTH_URL,
